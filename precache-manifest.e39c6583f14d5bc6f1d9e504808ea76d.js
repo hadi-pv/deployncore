c6898858de957666c55fecb0401f676f250db47a
hadi-pv <chumma1357@gmail.com>
2020-08-05 13:04:39 +0530

    Updates

---
+++ precache-manifest.e39c6583f14d5bc6f1d9e504808ea76d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146b6b54ee842180c3cffd6dfab7aaf7",
+    "url": "/deployncore/index.html"
+  },
+  {
+    "revision": "2cd44ca48cea37581a76",
+    "url": "/deployncore/static/css/2.6230457a.chunk.css"
+  },
+  {
+    "revision": "34cc1529295d7b9a40f9",
+    "url": "/deployncore/static/css/main.4501e556.chunk.css"
+  },
+  {
+    "revision": "2cd44ca48cea37581a76",
+    "url": "/deployncore/static/js/2.e71494de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/deployncore/static/js/2.e71494de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34cc1529295d7b9a40f9",
+    "url": "/deployncore/static/js/main.d881cdf2.chunk.js"
+  },
+  {
+    "revision": "ee8b0572c57615761d5f",
+    "url": "/deployncore/static/js/runtime-main.4135f639.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/deployncore/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/deployncore/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/deployncore/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/deployncore/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/deployncore/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
